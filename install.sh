bdb32402f094b30c561f0f471319d0aad8b312fd
joaogodinho <joaogodinho@users.noreply.github.com>
2025-02-27 12:30:09 +0000

    Feat: Remove vim stuff, update alacritty

--- install.sh
+++ install.sh
@@ -7,12 +7,5 @@ readonly FULLDIR="$(sed "s/\/\.$//" <(echo -n $DIRNAME))"
 echo "Linking tmux config..."
 ln -i -s $FULLDIR/tmux.conf $HOME/.tmux.conf
 
-echo "Linking vimrc config..."
-ln -i -s $FULLDIR/vimrc $HOME/.vimrc
-
-echo "Linking vim config folder..."
-rm -r -i $HOME/.vim
-ln -i -s $FULLDIR/vim $HOME/.vim
-
 echo "Linking alacritty config..."
-ln -i -s $FULLDIR/alacritty.yml $HOME/.alacritty.yml
+ln -i -s $FULLDIR/alacritty.toml $HOME/.alacritty.toml
